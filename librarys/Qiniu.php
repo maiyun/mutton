b03e66ec97f77046ebb6f1c5450cee7469f3d51b
HanGuoshuai <zohegs@163.com>
2015-08-17 11:54:27 +0800

    修复一些有关 qiniu 类的 bug

--- librarys/Qiniu.php
+++ librarys/Qiniu.php
@@ -12,12 +12,21 @@ class Qiniu {
 
     // --- 获取直传 token ---
     public function getToken($json = []) {
+
         $putPolicy = json_encode($json);
-        $encodedPutPolicy = urlsafe_base64_encode($putPolicy);
-        $sign = hmac_sha1($encodedPutPolicy, QINIU_SECRETKEY);
-        $encodedSign = urlsafe_base64_encode($sign);
-        $uploadToken = QINIU_ACCESSKEY + ':' + $encodedSign + ':' + $encodedPutPolicy;
+        $encodedPutPolicy = $this->base64UrlSafeEncode($putPolicy);
+        $sign = hash_hmac('sha1', $encodedPutPolicy, QINIU_SECRETKEY, true);
+        $encodedSign = $this->base64UrlSafeEncode($sign);
+        $uploadToken = QINIU_ACCESSKEY . ':' . $encodedSign . ':' . $encodedPutPolicy;
         return $uploadToken;
+        //*/
+    }
+
+    // --- url 安全 base64 ---
+    public function base64UrlSafeEncode($data) {
+        $find = array('+', '/');
+        $replace = array('-', '_');
+        return str_replace($find, $replace, base64_encode($data));
     }
 
 }
