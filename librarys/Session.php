3551ef8be669b7787ed0fd073c492b6874c7b92d
HanGuoshuai <zohegs@163.com>
2015-11-17 19:38:41 +0800

    移除 Qiniu 类 Session 类支持先到内存里读，未命中去数据库读

--- librarys/Session.php
+++ librarys/Session.php
@@ -10,122 +10,99 @@ namespace Chameleon\Library;
 
 /*
  * 若使用 Db 模式，则需要手动载入 Db 类
- * 若使用 Memcached 模式，则需要手动载入 Memcached 类
+ * 若使用默认模式，则需要同时手动载入  Memcached 类和 Db 类
  */
 
 class Session {
 
-    var $key = '';
+	var $key = '';
 
-    // --- 可编辑变量 ---
+	// --- 可编辑变量 ---
 
-    var $exp = 1209600;
-    var $source = NULL;
-    var $cookie = 'SESSIONKEY';
+	var $exp = 432000; // --- 5 天 ---
+	var $mem = true; // --- 内存加速 ---
+	var $cookie = 'SESSIONKEY';
 
-    function __construct() {
+	function __construct() {
 
+	}
 
+	function __destruct() {
 
-    }
+		L()->Db->query('UPDATE `' . L()->Db->pre . 'session` SET `data` = "' . L()->Db->escape(serialize($_SESSION)) . '",`time` = "' . time() . '" WHERE `key` = "' . $this->key . '"');
+		if($this->mem)
+			L()->Memcached->set('__sess_'.$this->key, $_SESSION, 43200); // 内存只保留 1 天的加速
 
-    function __destruct() {
+	}
 
-        if($this->source instanceof Db)
-            $this->source->query('UPDATE `' . $this->source->pre . 'session` SET `data` = "' . $this->source->escape(serialize($_SESSION)) . '",`time` = "' . time() . '" WHERE `key` = "' . $this->key . '"');
-        else if($this->source instanceof Memcached) {
-            $_SESSION['sess']['time'] = time();
-            $this->source->set('sess_'.$this->key, $_SESSION, $this->exp);
-        }
+	function gc() {
 
-    }
+		L()->Db->query('DELETE'.' FROM `' . L()->Db->pre . 'session` WHERE `time` < "'.(time() - $this->exp).'"');
 
-    function gc() {
+	}
 
-        if($this->source instanceof Db)
-            L()->Db->query('DELETE'.' FROM `' . $this->source->pre . 'session` WHERE `time` < "'.(time() - $this->exp).'"');
+	function start() {
 
-    }
+		if(isset($_POST[$this->cookie]) && $_POST[$this->cookie]) $this->key = $_POST[$this->cookie];
+		else if(isset($_COOKIE[$this->cookie]) && $_COOKIE[$this->cookie]) $this->key = $_COOKIE[$this->cookie];
+		if(!ctype_alnum($this->key)) $this->key = '';
 
-    function start() {
-
-        if(isset($_POST[$this->cookie]) && $_POST[$this->cookie]) $this->key = $_POST[$this->cookie];
-        else if(isset($_COOKIE[$this->cookie]) && $_COOKIE[$this->cookie]) $this->key = $_COOKIE[$this->cookie];
-        if(!ctype_alnum($this->key)) $this->key = '';
-
-        $_SESSION = [];
-        if($this->source instanceof Db) {
-            if($this->source->isConnected()) {
-                if($this->key != '')
-                    $r = $this->source->query('SELECT' . ' * FROM `' . $this->source->pre . 'session` WHERE `key` = "' . $this->key . '";');
-                // --- 影响 0 行代表之前没有 Session ---
-                if ($this->key =='' || $this->source->getAffectRows() == 0) {
-                    $this->key = date('Ymd') . $this->random();
-                    $time = time();
-                    while (!$this->source->query('INSERT' . ' INTO `' . $this->source->pre . 'session` (`key`,`data`,`time`,`time_add`) VALUES ("' . $this->key . '","a:0:{}","' . $time . '","' . $time . '")', false))
-                        $this->key = date('Ymd') . $this->random();
+        // --- 初始化 Session 数组 ---
+		$_SESSION = [];
+        // --- 有 key 则查看 key 的信息是否存在
+        $findOnDb = false;
+        $needInsert = false;
+		if($this->key != '') {
+            // --- 如果启用了内存加速则先在内存找 ---
+            if ($this->mem) {
+                $s = L()->Memcached->get('__sess_' . $this->key);
+                // --- 内存没有，一会儿去数据库再找找 ---
+                if ($s === false) {
+                    $findOnDb = true;
                 } else {
-                    $s = $r->fetch_assoc();
-                    if ($s['time_add'] < time() - 10800) {
-                        // --- 进行 Session KEY 置换 ---
-                        $this->key = date('Ymd') . $this->random();
-                        $time = time();
-                        while (!$this->source->query('INSERT'.' INTO `' . $this->source->pre . 'session` (`key`,`data`,`time`,`time_add`) VALUES ("' . $this->key . '","' . $this->source->escape(serialize($_SESSION)) . '","' . $time . '","' . $time . '")', false))
-                            $this->key = date('Ymd') . $this->random();
-                        $this->source->query('DELETE'.' FROM `' . $this->source->pre . 'session` WHERE `key` = "'.$s['key'].'"');
-                    }
-                    $_SESSION = unserialize($s['data']);
-                }
-                setcookie($this->cookie, $this->key, time() + $this->exp, '/');
-            } else {
-                logs('L(Session)', 'Db not connect', true);
-            }
-        } else if($this->source instanceof Memcached) {
-            if($this->source->isConnect()) {
-                $s = $this->source->get('sess_'.$this->key);
-                if($s === false) {
-                    // --- 没有 session ---
-                    $this->key = date('Ymd') . $this->random();
-                    $time = time();
-                    $_SESSION['sess'] = [
-                        'time' => $time,
-                        'time_add' => $time
-                    ];
-                    while($this->source->get('sess_'.$this->key) !== false)
-                        $this->key = date('Ymd') . $this->random();
-                    $this->source->set('sess_'.$this->key, $_SESSION, $this->exp);
-                } else {
-                    if ($s['sess']['time_add'] < time() - 10800) {
-                        // --- 进行 Session KEY 置换 ---
-                        $oldKey = $this->key;
-                        $this->key = date('Ymd') . $this->random();
-                        $time = time();
-                        while ($this->source->get('sess_'.$this->key) !== false)
-                            $this->key = date('Ymd') . $this->random();
-                        $s['sess'] = [
-                            'time' => $time,
-                            'time_add' => $time
-                        ];
-                        $this->source->set('sess_'.$this->key, $s, $this->exp);
-                        $this->source->delete('sess_'.$oldKey);
-                    }
+                    // --- 内存有直接读出 ---
                     $_SESSION = $s;
                 }
-                setcookie($this->cookie, $this->key, time() + $this->exp, '/');
-            } else
-                logs('L(Session)', 'Memcached not connect');
-        } else
-            logs('L(Session)', 'Please set source first');
+            } else {
+                $findOnDb = true;
+            }
+            // --- 本来就该在数据库里找 ---
+            // --- 在内存里没找到的也在数据库里找 ---
+            if($findOnDb) {
+                $r = L()->Db->query('SELECT' . ' * FROM `' . L()->Db->pre . 'session` WHERE `key` = "' . $this->key . '";');
+                // --- 影响 0 行代表之前没有 Session，一会儿添加个 ---
+                if (L()->Db->getAffectRows() == 0) {
+                    $needInsert = true;
+                } else {
+                    // --- 数据库里有 Session 直接读出 ---
+                    $s = $r->fetch_assoc();
+                    $_SESSION = unserialize($s['data']);
+                }
+            }
+        } else {
+            // --- 全新的机子 ---
+            $needInsert = true;
+        }
+        // --- 本来就该添加个新 Session ---
+        // --- 内存和数据库里没找到的也该添加个新 Session ---
+        if($needInsert) {
+            $this->key = date('Ymd') . $this->random();
+            $time = time();
+            while (!L()->Db->query('INSERT' . ' INTO `' . L()->Db->pre . 'session` (`key`,`data`,`time`,`time_add`) VALUES ("' . $this->key . '","a:0:{}","' . $time . '","' . $time . '")', false))
+                $this->key = date('Ymd') . $this->random();
+            // --- 如果内存加速了则在页面结束时再写入内存 ---
+        }
+        if(!isset($_POST[$this->cookie])) setcookie($this->cookie, $this->key, time() + $this->exp, '/');
 
-    }
+	}
 
-    protected function random() {
-        $s = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        $sl= strlen($s);
-        $t = '';
-        for ($i = 8; $i; $i--)
-            $t .= $s[rand(0, $sl - 1)];
-        return $t;
-    }
+	protected function random() {
+		$s = 'abcdefghijklmnopqrstuvwxyz0123456789';
+		$sl= strlen($s);
+		$t = '';
+		for ($i = 8; $i; $i--)
+			$t .= $s[rand(0, $sl - 1)];
+		return $t;
+	}
 
 }
