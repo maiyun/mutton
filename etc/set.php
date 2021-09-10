2890d685e73c2e933f238847907a91791c7bbc1c
jsqy <zohegs@gmail.com>
2021-09-10 12:08:11 +0800

    Op STATIC_PATH.

--- etc/set.php
+++ etc/set.php
@@ -2,12 +2,12 @@
 
 // --- BASE ---
 
-const STATIC_VER = '2020-3-24 15:27:21';
 const TIMEZONE = 'Asia/Shanghai';
 const MUST_HTTPS = false;
 const CACHE_TTL = 0;
 
 // --- STATIC ---
 
-const STATIC_PATH = "";
+const STATIC_VER = '2021-9-10 12:07:42';
+const STATIC_PATH = URL_STC;
 
